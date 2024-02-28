75a788c218337f060b64ed30f88f6b2e2fad1d60
Diego Araujo <diegodev03@gmail.com>
2024-02-28 12:17:42 -0300

    learning about request and HttpResponse

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+def my_view(request):
+    return HttpResponse('Uma linda string')
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('sobre/', my_view),
 ]
